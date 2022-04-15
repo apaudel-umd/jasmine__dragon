29b0595a823d76cb4c39c6ac3cd7b380638cf4c6
Adhish P <apaudel@terpmail.umd.edu>
2022-04-15 16:14:05 -0400

    Overhaul of multiple classes by Katherine

--- teahouse.py
+++ teahouse.py
@@ -1,33 +1,127 @@
+SIZES_PRICE = [("small", 2.50), ("medium", 4.00),("large", 5.50)]
+
 class TeaHouse:
     """A TeaHouse object which consists of customer objects, tea objects, and worker objects. (using composition)
       
+    Atrributes:
+        customers(list of Customer objects): the customers inside the TeaHouse
+        teas(list of Tea objects): teas that are available to order
+        workers(list of Worker objects): workers at the TeaHouse
     """
+    def __init__(self, name):
+        """Initialize a TeaHouse object.
+        
+        Args:
+            name(string): Name of the teaHouse
+        """   
+        pass
     
-    pass
+    def add_teas(self, tea):
+        """Add a new tea to the list teas available at this TeaHouse.
+
+        Args:
+            tea(Tea): new tea to add to the list of teas
+            
+        Returns:
+            The updated list of teas.
+        
+        Side effects:
+            Updating the teas attribute of TeaHouse.
+        """
+        pass
+    
+    def add_customers(self, customer):
+        """Add a new customer to the list customers at the TeaHouse.
+
+        Args:
+            customer(Customer): new customer to add to the list of customers
+            
+        Returns:
+            The updated list of customers.
+        
+        Side effects:
+            Updating the customers attribute of TeaHouse.
+        """
+        pass
+    
+    def sorting_customers(self, key):
+        """Sort the list of customers by the key provided.
+        
+        Args:
+            key(string): key function used to sort the list of customers a certain way.
+         
+        Returns:
+            The sorted list of customers.    
+        """
+        pass
+    
+    def current_customers(self):
+        """Look at the customers attribute and show the names of all the customers at the TeaHouse.
+        
+        Returns:
+            customer_names(string): f-string of all the customers at the TeaHouse.
+        """
+        pass
+    
+    def add_workers(self, worker):
+        """Add a new worker to the list workerss at the TeaHouse.
+
+        Args:
+            worker(Worker): new worker to add to the list of workers
+            
+        Returns:
+            The updated list of workers.
+        
+        Side effects:
+            Updating the workers attribute of TeaHouse.
+        """
+        pass
 
 class Customer:
     """A customer object.
     
      Attributes:
-        Customer.name (string): name of customer
-        Customer.order (string list): order of customer
-        Customer.received (string list): their order that the waiter will give to them after receiving their order
+        name (string): name of customer
+        money (float): how much money a customer has
+        order (set of tea): order of customer
+        received (set of tea): their order that the waiter will give to them after receiving their order
     """
-    def __init__(name, order, received):
+    def __init__(name, money):
         """Initialize a customer object.
         
         Args:
             name(string): name of customer
+            money (float): how much money a customer has
             order(string list): order of customer
             received (string list): their order that the waiter will give to them after receiving their order
         """
         pass
     
+    def __str__(self):
+        """Informal representation of a Customer object.
+        
+        The format of informal representation will be: 
+            "Customer Name: ___" + "Money:___ + "order:___" + "received:____ " 
+        
+        Return:
+            string of formal representation of the Customer object    
+        """
+        pass
+    
+    def __repr__(self):
+        """Formal representation of a Customer object.
+        
+        Return:
+            string of formal representation of the Customer object
+        """
+        
+        pass
+    
     def addOrder(order):
-        """Add a order to a customer object.
+        """Add a order to a customer object if customer has enough money.
         
         Args:
-            order(string list): order of customer
+            order(tea): order of customer
         """
         pass
     
@@ -35,7 +129,26 @@ class Customer:
         """Waiter has given a customer object their order.
         
         Args: 
-            received(string list): their order that the waiter will give to them after receiving their order
+            received(tea): their order that the waiter will give to them after receiving their order
+        """
+        pass
+    
+    def remaining_order(self):
+        """Compare the order set and received set to see how much of their order a customer still needs to receive.
+        
+        Return:
+            Set of the tea that the customer has ordered but hasn't received yet.
+        """
+        pass
+    
+    def pay_order(worker):
+        """Asks a worker for the bill and pays the amount (according to the received set).
+        
+        Return:
+            paid(string): states if the bill has been paid or not
+            
+        Side effect:
+            Changes the money attribute of the customer object.
         """
         pass
 
@@ -43,19 +156,19 @@ class Tea:
     """A Tea object.
     
      Attributes:
-        Tea.temp(string): Only ‘hot’ or ‘cold’    
-        Tea.type(string): The type of tea
-        Tea.size(string): small , medium, or large
-        Tea.price(float): The prices of the tea
-    """
-    def __init__(temp, type, size, price):
-        """Initialize a Tea object.
-        
-        Args:
-            temp(string): Only ‘hot’ or ‘cold’    
             type(string): The type of tea
-            size(string): small , medium, or large
-            price(float): The prices of the tea
+            price(float): The prices of the tea, which is determined by the size of tea.
+            temp(string): default value is "hot", Values of temp can only be: ‘hot’ or ‘cold’    
+            size(string): default value is ""medium, Values can only be: small , medium, or large
+    """
+    def __init__(type, temp = "hot", size = "medium"):
+        """Initialize a Tea object. 
+        
+        Args: 
+            type(string): The type of tea
+            temp(string): default value is "hot", Values of temp can only be: ‘hot’ or ‘cold’    
+            size(string): default value is ""medium, Values can only be: small , medium, or large
+            
         """
         pass
     
@@ -78,11 +191,14 @@ class Tea:
             
         Return:
             Current tea object.
+            
+        Side effects:
+            Changes the size and price attributes of the tea object.
         """
         pass
 
 class Worker:
-    """A Worker object.(There are two types of workers.)
+    """A Worker object.(There are two types of workers: Cashiers and Waiters.)
 
      Attributes:
         Worker.name(string): name of the worker
@@ -94,20 +210,39 @@ class Worker:
             name(string): name of the worker   
         """
         pass
+    def recommend_tea(teaHouse):
+        """Worker will recommend a random tea available at the TeaHouse.
+         
+        Args:
+            teaHouse(TeaHouse): the teaHouse that the worker works in.
+         
+        Return:
+            tea_rec(string): A tea recommendation
+        """
     
 
 class Cashier(Worker):
     """A Worker object.(There are two types of workers.)
 
      Attributes:
-        Cashier.cash(list of floats): the amounts of money the customer pays for the bill
+        name(string): name of the worker
     """
-    def __init__(name, cash):
-        """Initialize a Cashier object.
+    def __init__(name):
+        """Initialize a Cashier object. Only Cashiers can received payment from a customer.
         
         Args:
             name(string): name of the worker   
-            Cashier.cash(list of floats): the amounts of money the customer pays for the bill
+        """
+        pass
+    
+    def receive_payment(customer):
+        """Will take payment from a customer and return the any remaining money to the customer.
+        
+        Args:
+            customer(Customer): customer that wishes to pay their bill
+        
+        Return:
+            has_paid(string): String stating if the bill was paid or not and if the customer has any remaining money.
         """
         pass
 
@@ -115,22 +250,24 @@ class Waiter(Worker):
     """A Worker object.(There are two types of workers.)
 
      Attributes:
-        Worker.change(list of floats): the remaining money, the worker gives back to the customer
+        name(string): name of the worker
     """
-    def __init__(name, change):
-        """Initialize a Waiter object.
+    def __init__(name):
+        """Initialize a Waiter object. Waiters can only take orders of customers.
         
         Args:
             name(string): name of the worker   
-            change(list of floats): the remaining money, the worker gives back to the customer
         """
         pass
     
     def takeOrder(order):
-        """Takes order of a customer.
+        """Checks if a customer has enough money for their order then takes order of a customer if possible.
         
         Args:
-        
-            order:
+            order(string): order of a customer.
+            
+        Returns: 
+            The order has been taken or state that the customer does not have 
+                sufficient amount of money for their order.
         """
         pass
