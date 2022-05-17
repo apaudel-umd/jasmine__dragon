bff06625ac1618eefc382cb1537ed4bc6f662f4f
kargenta <kargenta@terpmail.umd.edu>
2022-05-17 12:51:04 -0400

    update Waiter run method

--- teahouse.py
+++ teahouse.py
@@ -521,9 +521,9 @@ class Waiter(Worker):
         return super().recommend_tea(teaHouse)
     
     def run(self, teahouse):
-        random_customer_dialogue = [("Hi my name is Holly, I would like a medium cold black tea."),
-                                  ("Hello I'm Allen, I would like small cold green tea with boba"),
-                                  ("Hi I'm Kim, can I get a large cold oolong tea with honey? Thanks")]
+        random_customer_dialogue = ["Hi my name is Holly, I would like a medium cold black tea.",
+                                  "Hello I'm Allen, I would like small cold green tea with boba",
+                                  "Hi I'm Kim, can I get a large cold oolong tea with honey? Thanks"]
         while True:
             print(f'''Hello, {self.name}. What would you like to do?\n
                   0 : Take order 
@@ -531,7 +531,10 @@ class Waiter(Worker):
                   2 : Exit''')
             choice = input('> ')
             if choice == '0':
-                print(choice(random_customer_dialogue))
+                #guest1 = choice(random_customer_dialogue)
+                print(random_customer_dialogue[1])
+                customer_name = input("Input name of customer:\n")
+                
                 pass #self.takeOrder('Jasmine Tea')
             elif choice == '1':
                 teahouse.plot_data()
